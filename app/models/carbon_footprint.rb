0424d333dbee8dee7e172b7d237bebe23a07e15c
abdubdoyo <abdubdoyo@gmail.com>
2024-08-14 01:53:14 +0300

    Changes

--- app/models/carbon_footprint.rb
+++
@@ -1,3 +0,0 @@
-class CarbonFootprint < ApplicationRecord
-    belongs_to :user 
-end
--- test/controllers/carbon_footprints_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CarbonFootprintsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
