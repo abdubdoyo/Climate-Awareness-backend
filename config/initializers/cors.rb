05e8ef7c371bb8dadaa89aaa4950f71f8acdbd1d
abdubdoyo <abdubdoyo@gmail.com>
2024-08-15 21:10:55 +0300

    Update CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "https://git.heroku.com/gentle-coast-92769.git"
+    origins "http://localhost:3001", "https://gentle-coast-92769.herokuapp.com"
 
     resource "*",
       headers: :any,
