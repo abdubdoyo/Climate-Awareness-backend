06bee46f589cb912396f2578cd599668f28de82c
abdubdoyo <abdubdoyo@gmail.com>
2024-12-19 08:00:27 +0300

    fixed login and signup functionality

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001", "https://gentle-coast-92769.herokuapp.com"
+    origins "http://localhost:3000", "https://gentle-coast-92769.herokuapp.com"
 
     resource "*",
       headers: :any,
